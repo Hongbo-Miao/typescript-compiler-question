6c4a8adbe4cc0148d37e3b9ca3eb97d95b92754c
Hongbo Miao <Hongbo.Miao@outlook.com>
2017-06-07 15:21:20 -0700

    feat: symbol type

--- app.ts
+++ app.ts
@@ -10,7 +10,10 @@ const sourceFile = program.getSourceFile(userAppFile);
 function printApi(node) {
   const symbol = checker.getSymbolAtLocation(node);
 
-  if (symbol) console.log(symbol.name);
+  if (symbol) {
+    const type = Object.keys(ts.SymbolFlags).find(k => Number(ts.SymbolFlags[k]) === Number(symbol.flags));
+    console.log(`${symbol.name} is ${type}`);
+  }
 
   ts.forEachChild(node, printApi);
 }
